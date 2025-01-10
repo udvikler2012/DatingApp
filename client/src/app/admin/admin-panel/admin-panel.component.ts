4e61160cb467753a83a1123b0fd0a250b98bd942
Louis Andersen <26446148+udvikler2012@users.noreply.github.com>
2025-01-10 22:46:40 +0100

    fix text-input

--- client/src/app/admin/admin-panel/admin-panel.component.ts
+++ client/src/app/admin/admin-panel/admin-panel.component.ts
@@ -5,6 +5,7 @@ import { HasRoleDirective } from '../../_directives/has-role.directive';
 import { PhotoManagementComponent } from "../photo-management/photo-management.component";
 
 @Component({
+    standalone:true,
     selector: 'app-admin-panel',
     imports: [TabsModule, UserManagementComponent, HasRoleDirective, PhotoManagementComponent],
     templateUrl: './admin-panel.component.html',
