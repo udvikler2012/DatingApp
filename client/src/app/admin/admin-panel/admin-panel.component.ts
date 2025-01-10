14711b583a26ec206ebb75caced9ffcf7eb22d48
Louis Andersen <26446148+udvikler2012@users.noreply.github.com>
2025-01-10 23:32:28 +0100

    update to angular 19

--- client/src/app/admin/admin-panel/admin-panel.component.ts
+++ client/src/app/admin/admin-panel/admin-panel.component.ts
@@ -5,7 +5,6 @@ import { HasRoleDirective } from '../../_directives/has-role.directive';
 import { PhotoManagementComponent } from "../photo-management/photo-management.component";
 
 @Component({
-    standalone:true,
     selector: 'app-admin-panel',
     imports: [TabsModule, UserManagementComponent, HasRoleDirective, PhotoManagementComponent],
     templateUrl: './admin-panel.component.html',
