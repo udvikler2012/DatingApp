89de6b09c5ac816888f27b8bd645ae2e13d83bec
Louis Andersen <26446148+udvikler2012@users.noreply.github.com>
2026-01-01 15:00:09 +0100

    refactor 2025 route-guards, toast

--- client/src/app/_models/user.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-    username: string
-    knownAs: string;
-    gender: string;
-    token: string;
-    photoUrl?: string;
-    roles:string[];
-}
--- client/src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl: 'https://localhost:5001/api/',
-    hubsUrl: 'https://localhost:5001/hubs/',
-};
-
